23b80a0cf6ac1034dd868865867e1d4ea70e4265
izebr <i.zebrowski.ul@gmail.com>
2022-05-12 17:17:32 +0200

    second

--- src/components/ClubView.vue
+++ src/components/ClubView.vue
@@ -14,8 +14,9 @@
         </thead>
       </template>
       <template v-slot="{ item, index }">
-        <tr @dblclick="open=true">
+            <tr @dblclick="open=true,clubItem = item,uuid=item.uuid, name= item.name, city=item.city">
           <td>{{ index+1 }}</td>
+          <td>{{ item.uuid }}</td>
           <td>{{ item.name }}</td>
           <td>{{item.city}}</td>
         </tr>
@@ -24,16 +25,51 @@
     </div>
     </div>
   </q-item>
-  <q-dialog v-model="open" @keypress.enter="open=false">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">coś będzie zmieniane</div>
+<q-dialog v-model="open" @keypress.esc="open=false">
+      <q-card class="full-width">
+        <q-card-section class="col full-width">
+          <div class="row">
+          <div class="col">
+            <div class="text-h6">ID: {{clubItem.uuid}}</div>
+            <div class="text-h6">Nazwa: {{clubItem.name}}</div>
+            <div class="text-h6">Miasto: {{clubItem.city}}</div>
+          </div>
+          <div class="col">
+            <div><q-input class="full-width" v-model="name" dense filled color="primary" stack-label label="Nazwa"></q-input></div>
+            <div><q-input class="full-width" v-model="city" dense filled color="primary" stack-label label="Miasto"></q-input></div>
+          </div>
+          </div>
         </q-card-section>
 
         <q-card-actions align="right">
-          <q-btn flat label="zamknij" color="primary" v-close-popup />
-          <q-btn flat label="Zapisz" color="primary" v-close-popup />
+          <q-btn flat label="Anuluj" color="primary" v-close-popup @click="clubItem=null"/>
+          <q-btn flat label="Zmień" color="primary" v-close-popup  @click="updateClub()" />
         </q-card-actions>
+      </q-card>
+</q-dialog>
+<q-dialog :position="'top'" v-model="failure">
+      <q-card>
+        <q-card-section>
+          <div v-if="message!=null" class="text-h6">{{message}}</div>
+          <div v-else class="text-h6">Nie można wykonać żądania. Sprawdź poprawność danych.</div>
+        </q-card-section>
+
+      </q-card>
+</q-dialog>
+<q-dialog :position="'top'" v-model="success">
+      <q-card>
+        <q-card-section>
+          <div v-if="message!=null" class="text-h6">{{message}}</div>
+        </q-card-section>
+
+      </q-card>
+</q-dialog>
+<q-dialog :position="'top'" v-model="forbidden">
+      <q-card class="bg-warning">
+        <q-card-section>
+          <div class="text-h6">Niewłaściwy kod. Spróbuj ponownie.</div>
+        </q-card-section>
+
       </q-card>
 </q-dialog>
 </template>
@@ -52,8 +88,15 @@ export default defineComponent({
   data() {
     return {
       clubList: [],
+      uuid: "",
+      name: "",
+      city: "",
+      clubItem: null,
       open: false,
-      local: App.host,
+      failure: false,
+      success: false,
+      forbidden: false,
+      local: App.host
     };
   },
   methods: {
@@ -67,6 +110,22 @@ export default defineComponent({
         response.json().then((response) => {
           this.clubList = response;
         });
+      });
+    },
+    updateClub() {
+      const data = {
+        uuid: this.uuid,
+        name: this.firstName,
+        city: this.secondName
+      }
+      fetch("http://" + this.local + "/club/", {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then(() => {
+        this.getAllClubs()
       });
     },
   }
--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -19,7 +19,7 @@
         </thead>
           </template>
           <template v-slot="{ item, index }">
-            <tr @dblclick="open=true">
+            <tr @dblclick="open=true,playerItem = item,uuid=item.uuid, firstName= item.firstName, secondName=item.secondName,licenseNumber=item.licenseNumber, vintage = item.vintage, playerClubName=item.club.name, startNumber= item.startNumber">
               <td>{{index+1}}</td>
               <td>{{item.secondName}}</td>
               <td>{{item.firstName}}</td>
@@ -27,7 +27,7 @@
               <td>{{item.vintage}}</td>
               <td>tutaj trzeba dorobić kategorię wiekową</td>
               <td>{{item.startNumber}}</td>
-              <td>{{item.club.name}}</td>
+              <td>{{item.club.name}} {{item.club.city}}</td>
             </tr>
           </template>
     </q-virtual-scroll>
@@ -40,15 +40,64 @@
     <q-item>
       <q-btn label="generuj csv" @click="generateCSV()"></q-btn>
     </q-item>
-      <q-dialog v-model="open" @keypress.enter="open=false">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">coś będzie zmieniane</div>
+      <q-dialog @keypress.enter="open=false" style="width:80vw">
+      <q-card style="width:60vw;height:60vh">
+          <q-card-section class="row items-center q-pb-none">
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
         </q-card-section>
-
+        <div class="row" style="height:85%;">
+        <q-card-section class="col">
+          <div>
+            <div class="text-h6">Nazwisko i imię:{{playerItem.secondName}} {{playerItem.firstName}}</div>
+            <div class="text-h6">Numer startowy:{{playerItem.startNumber}}</div>
+            <div class="text-h6">Numer licencji:{{playerItem.licenseNumber}}</div>
+            <div class="text-h6">rocznik:{{playerItem.vintage}}</div>
+            <div class="text-h6">Klub:{{playerItem.club.name}} {{playerItem.club.city}}</div>
+          </div>
+        </q-card-section>
+        <q-card-section class="col">
+          <div>
+            <div><q-input class="full-width" filled label="coś" color="primary" stack-label></q-input></div>
+            <div>{{playerItem.startNumber}}</div>
+            <div>{{playerItem.licenseNumber}}</div>
+            <div>{{playerItem.vintage}}</div>
+            <div>{{playerItem.club.name}} {{playerItem.club.city}}</div>
+          </div>
+        </q-card-section>
+        </div>
         <q-card-actions align="right">
           <q-btn flat label="zamknij" color="primary" v-close-popup />
           <q-btn flat label="Zapisz" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+</q-dialog>
+<q-dialog v-model="open" @keypress.esc="open=false">
+      <q-card class="full-width">
+        <q-card-section class="col full-width">
+          <div class="row">
+          <div class="col">
+            <div class="text-h6">Nazwisko: {{playerItem.secondName}}</div>
+            <div class="text-h6">Imię: {{playerItem.firstName}}</div>
+            <div class="text-h6">Numer startowy: {{playerItem.startNumber}}</div>
+            <div class="text-h6">Numer licencji: {{playerItem.licenseNumber}}</div>
+            <div class="text-h6">rocznik: {{playerItem.vintage}}</div>
+            <div class="text-h6">Klub: {{playerItem.club.name}} {{playerItem.club.city}}</div>
+          </div>
+          <div class="col">
+            <div><q-input class="full-width" v-model="secondName" dense filled color="primary" stack-label label="Nazwisko"></q-input></div>
+            <div><q-input class="full-width" v-model="firstName" dense filled color="primary" stack-label label="Imię"></q-input></div>
+            <div><q-input class="full-width" v-model="startNumber" dense filled color="primary" stack-label label="Numer Startowy"></q-input></div>
+            <div><q-input class="full-width" v-model="licenseNumber" dense filled color="primary" stack-label label="Numer Licencji"></q-input></div>
+            <div><q-input class="full-width" v-model="vintage" dense filled color="primary" stack-label label="Rocznik"></q-input></div>
+            <div><q-select class="full-width" v-model="playerClubName" :options="allClubsNames" use-input dense filled color="primary" stack-label label="Klub"></q-select></div>
+          </div>
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Anuluj" color="primary" v-close-popup @click="playerItem=null"/>
+          <q-btn flat label="Zmień" color="primary" v-close-popup  @click="updatePlayer(playerClubName)" />
         </q-card-actions>
       </q-card>
 </q-dialog>
@@ -66,24 +115,46 @@ export default defineComponent({
   name: "PlayerView",
   created() {
     this.getAllPlayers()
+    this.getAllClubNames()
   },
   data() {
     return {
+      uuid: "",
+      firstName: "",
+      secondName: "",
+      startNumber: "",
+      licenseNumber: "",
+      vintage: "",
+      playerClubName: null,
+      allClubsNames: [],
       playerList: [],
       open: false,
+      playerItem: null,
       local: App.host
     };
   },
   methods: {
     getAllPlayers() {
       fetch("http://" + this.local + "/player/", {
-        method: "Get",
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
       }).then((response) => {
         response.json().then((response) => {
           this.playerList = response;
+        });
+      });
+    },
+    getAllClubNames() {
+      fetch("http://" + this.local + "/club/names", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => {
+        response.json().then((response) => {
+          this.allClubsNames = response;
         });
       });
     },
@@ -100,6 +171,25 @@ export default defineComponent({
         document.body.appendChild(fileLink)
         fileLink.click()
       })
+    },
+    updatePlayer(club) {
+      const data = {
+        uuid: this.uuid,
+        firstName: this.firstName,
+        secondName: this.secondName,
+        licenseNumber: this.licenseNumber,
+        vintage: this.vintage,
+        startNumber: this.startNumber,
+      }
+      fetch("http://" + this.local + "/player/?club=" + club, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then(() => {
+        this.getAllPlayers()
+      });
     },
     generateCSV () {
       axios({
